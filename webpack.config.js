3657520a9983e6606f1099b8da55cdcc4eb5fe27
田刚刚 <tianganggang@tgg.local>
2021-12-20 11:43:52 +0800

    webpackcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  template: path.join(__dirname, "./example/src/index.html"),
-  filename: "./index.html"
-});
 
 module.exports = {
   entry: path.join(__dirname, "./src/index.js"),
@@ -35,35 +31,11 @@ module.exports = {
         exclude: /node_modules/
       },
       {
-        test: /\.(sa|sc|le)ss$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            },
-          },
-          'postcss-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'less-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
     ]
   },
-  plugins: [htmlWebpackPlugin],
   resolve: {
     extensions: [".js", ".jsx"]
   },
